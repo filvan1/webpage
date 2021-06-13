bd6cf3416a2f20ab830a981a9ca9e8356f6babc2
Filip Vannfält <filip.vannfalt@gmail.com>
2021-06-13 14:28:50 +0100

    removed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import React from 'react'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Home from "./components/Home";
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,9 @@
 import React from 'react'
-import "bootstrap/js/src/collapse.js";
-import "bootstrap/js/src/dropdown.js";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-
-            <div className="container-fluid">
-
-                <a className="navbar-brand" href="#">Navbar</a>
-
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link" href="about">About</a>
-                        </li>
-
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Projects
-                            </a>
-                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="game">Game</a></li>
-                                <li><a className="dropdown-item" href="#">Other</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="#">More...</a></li>
-                            </ul>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link second" href="#" tabindex="-1" >Resume</a>
-                        </li>
-                    </ul>
-
-
-                </div>
-
-            </div>
-        </nav>
-    )
+        <div></div>
+            )
 }
 
 export default Navbar
